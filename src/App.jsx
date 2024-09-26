20fe5f0ec986f53f3f82369d3ecde5c3696ee30c
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-27 00:51:22 +0530

    Input logic improved

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import ReactMarkdown from 'react-markdown';
 
 export default function App() {
 
   const[answer,setAnswer]=useState([]);
-  const[Query,setQuery]=useState([]);
+  const[question,setQuestion]=useState([]);
+  const[Query,setQuery]=useState('');
 
 
   async function GenerateAnswer(Query){
       console.log('Loading ...');
+      setQuestion((prev)=>[...prev,Query])
 
       try{
-      const response= await axios({
-        url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyA_ANFzW0lwaYaJKE_dnUnYu6vTPsjV7AU",
+          const response= await axios({
+            url:"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyA_ANFzW0lwaYaJKE_dnUnYu6vTPsjV7AU",
+          
+            method:"post",
+            data:{
+              "contents":[{"parts":[{"text":`${Query}`}]}]
+            }
+          })
 
-        method:"post",
-        data:{
-          "contents":[{"parts":[{"text":`${Query}`}]}]
-        }
-      })
-      
-      const data=response['data']['candidates'][0]['content']['parts'][0]['text']
-      setAnswer((prev)=>[...prev,data])
-    }
+          const data=response.data.candidates[0].content.parts[0].text;
+          setAnswer((prev)=>[...prev,data])
+      }
 
     catch(err){
       return <div>Some error occured...</div>
@@ -34,18 +37,28 @@ export default function App() {
   }, [answer]);
 
   return (
-    <div>
+    <div className='p-4'>
       <p>AI chat app</p>
 
       <form onSubmit={(e)=>e.preventDefault()}>
-        <input type="text" placeholder='Enter your query' className='border' onChange={(e)=>setQuery(()=>e.target.value)}/>
+        <input type="text" placeholder='Enter your query' className='border' value={Query} onChange={(e)=>setQuery(e.target.value)}/>
         <button onClick={()=>GenerateAnswer(Query)} className='bg-yellow-400 p-2'>Generate</button>
       </form>
-      {
-        
-          answer.map((item,index)=> (<> <p key={index} className='text-blue-500 text-lg'>{item}</p> <br /> <br /> </>))
-         
-      }
+
+        {
+             question.map((item, index) => (
+               <h1 className='text-red-500' key={index}>{item}</h1>
+             ))
+        }
+
+        {
+            answer.map((item, index) => (
+              <ReactMarkdown key={index} className='text-blue-500 text-lg mb-4'>
+                {item}
+              </ReactMarkdown>
+            ))
+        }
+
     </div>
   )
 }
