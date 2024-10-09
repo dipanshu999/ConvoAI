4bd45a0d12d205e8392f1a82aaa3325711bfeb71
Dipanshu Pandey <dps.coders@gmail.com>
2024-10-09 15:21:09 +0530

    Styling improved

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,8 +18,9 @@ export default function Sidebar() {
 
   return (
     <div className='bg-black fixed h-full w-[20vw] p-3  hidden tab:inline shadow-xl shadow-slate-500'>
-      <div className='Logo w-full h-52 '>
-        <p className='text-white'>Nav</p>
+      <div className='Logo w-full h-40 '>
+        <img src='../logo.jpg' className='h-20 rounded-xl mx-auto mt-2'/>
+        <p className='text-white justify-center text-center text-2xl font-semibold mt-3'>Convo AI</p>
       </div>
 
         <hr />
@@ -30,9 +31,10 @@ export default function Sidebar() {
           <button onClick={()=>navigate('/')} className=' bg-orange-500 w-12  p-2 rounded-full mb-4'><img src="../home.svg" alt="" /></button>
         </div>
 
-        
-        { chatHistory.length>0 ? (chatHistory.map((chat) => (
-          <div key={chat.id} className='w-full mb-2 bg-slate-700 p-2  rounded-md hover:shadow-md h-12 overflow-hidden hover:scale-95 duration-200 hover:shadow-slate-400'>
+      <div className="history-container h-[19rem] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-300 scroll-smooth">
+        {
+         chatHistory.length>0 ? (chatHistory.map((chat) => (
+          <div key={chat.id} className='w-full h-12 mb-2 bg-slate-700 p-2  rounded-md hover:shadow-md  overflow-hidden hover:scale-95 duration-200 hover:shadow-slate-400'>
             <button 
               className='text-white text-xl hover:bg-gray-700  block w-full text-left'
               onClick={() => handleChatClick(chat.id)}
@@ -40,7 +42,12 @@ export default function Sidebar() {
               { chat?.title && chat.title.split(' ').slice(0, 5).join(' ').replace(/^./, str => str.toUpperCase()) + (chat.title.split(' ').length >= 5 ? ' ...' : '') }
             </button>
           </div>
-        ))) : <div className='bg-slate-700 py-24 mt-6 rounded-xl'> <p className='text-red-500 font-bold text-[1.7em] text-center'>!! No chat history</p> </div>}
+        ))) 
+
+        : <div className='bg-slate-700 py-24 mt-6 rounded-xl'> <p className='text-red-500 font-bold text-[1.7em] text-center'>!! No chat history</p> </div>
+        
+        }
+        </div>
       </div>
     </div>
   )
